e9549e17ad8f80b6e7c31fb444f7e43d9b2eca81
marcoperuzzocmd <marcoantperuzzo@gmail.com>
2024-12-18 23:41:51 -0300

    feat

--- heiwaBackend/settings.py
+++ heiwaBackend/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 }
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
